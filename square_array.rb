df6e0c60b44cc0a1b7f9dc78b41ba945b70aea82
Stela Capsa <stela.capsa@gmail.com>
2019-11-15 16:31:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,11 @@
 def square_array(array)
   array.each do |numbers|
     puts "Here is every #{numbers} "
+  
+  end
+   self.map!{|numbers| numbers ** 2 }
   end
  end
 
-class square_array(numbers)
-  def square!
-     self.map!{|numbers| numbers ** 2 }
-  end
-end
 
 [1,2,3].square!
