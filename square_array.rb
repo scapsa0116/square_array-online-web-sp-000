2c4730038c6a0191bfc82b1c0d1ca0e3361e91d1
Stela Capsa <stela.capsa@gmail.com>
2019-11-15 15:36:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
-end
+  array.each do |numbers|
+    puts "Here is every #{numbers} "
+  end
+ numbers = [1,2,3]
+ 
+ square_array(numbers)
+
+  new_numbers = [9,10,16,25]
+ 
+  square_array(new_numbers)
+   collect/map
+   end
+
